30beca579eec081c7180101f6a6afb7c7287ed46
jetsaus <phpsaus@gmail.com>
2019-11-08 17:28:48 +0800

    1.1.Даны два числа 5 и 7. Найти их сумму и произведение.

---
+++ 1/1.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Задача 1</title>
+</head>
+<body>
+    <h3>Задача 1:</h3>
+    <p>Даны два числа 5 и 7. Найти их сумму и произведение.</p>
+    <h3>Решение:</h3>
+    <?php
+        $a = 5;
+        $b = 7;
+        $c = $a + $b;
+        $d = $a * $b;
+        echo "$a + $b = $c";
+        echo '<br>';
+        echo "$a * $b = $d";
+    ?>
+</body>
+</html>
