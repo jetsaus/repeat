e16bec9c381d13cb4837b2c6b49e553b54ed1553
jetsaus <phpsaus@gmail.com>
2019-11-08 17:25:40 +0800

    Инициализировал репозиторий для выполнения задач

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Повторение пройденного материала</title>
+</head>
+<body>
+    <h3>Задачник по PHP.</h3>
+    <h4><i>А.Григорьев, 2018</i></h4>
+    <p><a href="1/1.php" target="_blank">1.Даны два числа 5 и 7. Найти их сумму и произведение.</a></p>
+    <p><a href="2/2.php" target="_blank">2.Даны два числа 4 и 6. Найдите сумму их квадратов.</a></p>
+    <p><a href="3/3.php" target="_blank">3.Даны три числа 3, 5, 8. Найдите их среднее арифметическое.</a></p>
+    <p><a href="4/4.php" target="_blank">4.Даны три числа x = 2,y = 6 и z = 9. Найдите (x+1)4−2(z−2x^2+y^2).</a></p>
+    <p><a href="5/5.php" target="_blank">5.Даны три ненулевых числа $a = 4, $b = 8, $c = 3. Найдите всевозможные результаты деления суммы двух из них на оставшееся третье число.</a></p>
+    <p><a href="6/6.php" target="_blank">6.Дано два числа 17 и 54. Найдите сумму 40% от первого числа и 84% от второго числа.</a></p>
+    <p><a href="7/7.php" target="_blank">7.Дано трехзначное числа. Найдите сумму его цифр.</a></p>
+
+</body>
+</html>
