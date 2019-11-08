8b21d3b4b526a2d53ca8f1d1313d1d252b5819bc
jetsaus <phpsaus@gmail.com>
2019-11-08 17:58:39 +0800

    7.0.Дано трехзначное число. Найдите сумму его цифр.

---
+++ 7/7.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Задача 7</title>
+</head>
+<body>
+    <h3>Задача 7:</h3>
+    <p>Дано трехзначное числа. Найдите сумму его цифр.</p>
+    <?php
+        // Инициализируем 3-х значное число
+        $number = 347;
+
+    ?>
+    
+</body>
+</html>
