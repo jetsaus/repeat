3e1247ed3c285aead0e1db82628b00ceefdc86da
jetsaus <jetsaus@gmail.com>
2019-11-27 16:28:17 +0800

    14.3.Вывод дня недели получился в отдельную страницу, а не в поле формы!

--- 14/14.php
+++ 14/14.php
@@ -1,2 +1,28 @@
 <?php
-    echo 'Обработали!';
+if (!empty($_POST['week'])) {         // Если введены данные
+    {
+        switch ($_POST['week']) {
+            case 1:
+                echo 'Понедельник';
+                break;
+            case 2:
+                echo 'Вторник';
+                break;
+            case 3:
+                echo  'Среда';
+                break;
+            case 4:
+                echo  'Четверг';
+                break;
+            case 5:
+                echo  'Пятница';
+                break;
+            case 6:
+                echo 'Суббота';
+                break;
+            case 7:
+                echo 'Воскресенье';
+                break;
+        }
+    }
+}
