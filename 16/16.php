8775f681d4b1e8df94aa924e936acb0012c0b2d1
jetsaus <jetsaus@gmail.com>
2019-12-11 15:18:36 +0800

    16.3.Расчет стоимости отдыха

--- 16/16.php
+++ 16/16.php
@@ -1,5 +1,27 @@
 <?php
-    // Получение данных их формы    
-    $place = $_POST['place'];
-    $days = $_POST['days'];
-    $discount = $_POST['discount'];
+    // Если параметры введены
+    if ((isset($_POST['place'])) && (isset($_POST['days'])) && (isset($_POST['discount'])))
+    {
+        // Получение данных их формы    
+        $place = $_POST['place'];
+        $days = $_POST['days'];
+        $discount = $_POST['discount'];
+        $cost = $days * 400;
+        // Увеличение цены взависимости от страны
+        switch ($place) {
+            case'Египет':
+                $cost = $cost + ($cost/100*10);
+            break;
+            case 'Италия':
+                $cost = $cost + ($cost/100*12);
+            break;
+        }
+        // Уменьшение цены на скидку
+        if ($discount === 'true') {
+            $cost = $cost - ($cost/100*5);
+        }
+        echo "Стоимость отдыха - $cost";
+    } else {
+        echo 'Параметры не определены';
+        die;
+    }
