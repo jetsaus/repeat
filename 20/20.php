bbadee8c1c757e069ddd7f103ce7f722e663219e
jetsaus <phpsaus@gmail.com>
2020-02-11 13:22:52 +0800

    20.1.Дан массив с числами.Запишите в новый массив только те числа, в которых есть цифра 5.

---
+++ 20/20.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Задача 20</title>
+</head>
+<body>
+    <h3>Задача 20:</h3>
+    <p>Дан массив с числами. Запишите в новый массив только те числа, в которых есть цифра 5.</p>
+    <h3>Решение:</h3>
+    <?php
+        // Массивы чисел
+        $arrayIn = [            // Исходный массив
+
+        ];
+        $arrayOut = [];         // Результирующий массив
+    ?>
+</body>
+</html>
