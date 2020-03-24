62a336ab5f02b267ecf426504e53bc469fbcb36a
jetsaus <phpsaus@gmail.com>
2020-03-24 23:27:23 +0800

    23.1.Создана заготовка для задачи

---
+++ 23/23.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Задача 23</title>
+</head>
+<body>
+    <h3>Задача 23:</h3>
+    <p>Дан массив вида [1, ' , 2, ' , ' , 3] – то есть в нем есть пустые строки. Удалите все такие элементы из этого массива.*</p>
+    <h3>Решение:</h3>
+    <?php
+
+    ?>
+</body>
+</html>
