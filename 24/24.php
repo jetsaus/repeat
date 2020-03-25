45cac9c76914c8b39174aab8814f7fa8ac03b448
jetsaus <phpsaus@gmail.com>
2020-03-25 23:52:11 +0800

    24.3.Создана форма ввода времени

--- 24/24.php
+++ 24/24.php
@@ -11,7 +11,22 @@
     <p>Задача «Сложение времени»****:</p>
        Напишите функцию, которая корректно будет складывать часы и минуты. Примеры: на вход функции подается такое – 1ч20мин+50мин – в результатте функция выведет 2ч10мин.*** (Задача решается разными способами, приведите пример одного из них)
         <h3>Решение:</h3>
+        <!-- Форма ввода слагаемых времени -->
+        <form method="post">
+            <input type="number" size="3" name="lH" min="1" max="999" value="<?php if (isset($lH)) echo $lH;?>"  required>
+            <span>час.</span>
+            <input type="number" size="3" name="lM" min="1" max="999" value="<?php if (isset($lM)) echo $lM;?>"  required>
+            <span>мин.</span>
+            <b>+</b>
+            <input type="number" size="3" name="rH" min="1" max="999" value="<?php if (isset($rH)) echo $rH;?>"  required>
+            <span>час.</span>
+            <input type="number" size="3" name="rM" min="1" max="999" value="<?php if (isset($rM)) echo $rM;?>"  required>
+            <span>мин.</span>
+            <b>=</b>
+
+        </form>                    
         <?php
+            // Обработчик сложения времени
             require_once('24f.php');
             echo addTime(2, 23, 1, 50);
         ?>
