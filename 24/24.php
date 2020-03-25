eb82859aaceb336f7fc490a05e67bc9a1e3d6379
jetsaus <phpsaus@gmail.com>
2020-03-25 19:44:33 +0800

    24.1.Создана заготовка файлов для выполнения задачи

---
+++ 24/24.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Задача 24</title>
+</head>
+<body>
+    <h3>Задача 24:</h3>
+    <p>Задача «Сложение времени»****:</p>
+       Напишите функцию, которая корректно будет складывать часы и минуты. Примеры: на вход функции подается такое – 1ч20мин+50мин – в результатте функция выведет 2ч10мин.*** (Задача решается разными способами, приведите пример одного из них)
+        <h3>Решение:</h3>
+</body>
+</html>
