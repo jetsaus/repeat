25874e81e502ac6f962f1ff5f9fdce1ec4b008fb
jetsaus <phpsaus@gmail.com>
2020-03-25 18:38:37 +0800

    23.3.Из массива удалены пустые элементы(с подсказкой автора)

--- 23/23.php
+++ 23/23.php
@@ -11,12 +11,8 @@
     <p>Дан массив вида [1, ' , 2, ' , ' , 3] – то есть в нем есть пустые строки. Удалите все такие элементы из этого массива.*</p>
     <h3>Решение:</h3>
     <?php
-        // Функция обратного вызова, возвращает true, если элемент массива не пусть и не 0
-        function isNotEmpty($var) {
-            return !empty($var);
-        }
         // Исходный массив
-        $array = [1, 2, 3, '', '', 6, 7, '', 9, ''];
+        $array = [1, 2, 3, '', '', 6, 7, '', 9];
         echo '<p><i>Исходный массив: </i></p>';
         foreach ($array as $arrEl) {
             if ($arrEl == '') {
@@ -24,8 +20,9 @@
             }
             echo $arrEl . ', ';
         }
-        // Из массива удалены пустые элементы
-        $arrayNotEmpty = array_filter($array, "isNotEmpty");
+        // Удаление пустые элементы из массива
+        $arrayNotEmpty = array_filter($array);
+
         echo '<p><i>Массив без пустых элементов: </i></p>';
         foreach ($arrayNotEmpty as $arrEl) {
             if ($arrEl == '') {
