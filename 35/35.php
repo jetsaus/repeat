f2ac7422f65d6286b8d4c0305450e0c1145e322e
jetsaus <phpsaus@gmail.com>
2020-04-09 15:01:59 +0800

    35.Многомерный массив создан, необходимые слова выведены в браузер

---
+++ 35/35.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Задача 34</title>
+</head>
+<body>
+ . . 
+
+    <h3>Задача 35:</h3>
+    <p><i>Создайте многомерный массив $arr.
+    <pre>
+        $arr = ['cms'=>
+                ['joomla', 'wordpress', 'drupal'],
+                'colors'=>
+                ['blue'=>'голубой', 'red'=>'красный', 'green'=>'зеленый'],
+        ];
+    </pre>
+            С его помощью выведите на экран слова 'joomla', 'drupal', 'зеленый', 'красный’.</i></p>
+
+    <h3>Решение:</h3>
+    <p><i>Создан массив:</i></p>
+    <?php
+        $arr = ['cms'=>
+                ['joomla', 'wordpress', 'drupal'],
+                'colors'=>
+                ['blue'=>'голубой', 'red'=>'красный', 'green'=>'зеленый'],
+        ];
+        
+       var_dump($arr);
+       echo '<p><i>Вывод слов:</i></p>';
+       echo $arr['cms'][0] . '<br>';
+       echo $arr['cms'][2] . '<br>';
+       echo $arr['colors']['green'] . '<br>';
+       echo $arr['colors']['red'] . '<br>';
+    ?>
+</body>
+</html>
