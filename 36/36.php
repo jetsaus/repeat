02ea9ccbe261ae6733f7acca7afc97accc7525d0
jetsaus <phpsaus@gmail.com>
2020-04-09 15:34:36 +0800

    36.1.Создана заготовка для выполнения задачи

---
+++ 36/36.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Задача 36</title>
+</head>
+<body>
+ . . 
+
+    <h3>Задача 36:</h3>
+    <p><i>Создайте двухмерный массив. Первые два ключа – это 'ru' и 'en' . Пусть первый ключ содержит элемент, являющийся массивом названий дней недели по-русски, а второй – по-английски. Выведите с помощью этого массива понедельник по-русски и среду по английски (пусть понедельник – это первый день).</i></p>
+
+    <h3>Решение:</h3>
+    <p><i>Создан массив:</i></p>
+    <?php
+        $arr = ['ru' =>
+                ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Восресенье'],
+                'en' =>
+                ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
+        ];
+        
+       var_dump($arr);
+    ?>
+</body>
+</html>
