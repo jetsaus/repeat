f360ad06706cf72e066a903f1db0b5a1779b40eb
jetsaus <phpsaus@gmail.com>
2020-07-23 12:21:18 +0800

    39.1 Не выполнено! Не понятна постановка задачи

---
+++ 39/39.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Задача 38</title>
+</head>
+<body>
+    <h3>Задача 39:</h3>
+    <p><i>Дан многомерный массив. Уровни вложенности его подмассивов могут быть разными, например: $arr = array( array( 1, 2, 3, ), array( array(4, 5, array(6, 7, 7)), 9, 10, ), ). Выведите на экран столбец всех его элементов.</i></p>
+
+    <h3>Решение:</h3>
+    <?php
+        echo 'Не выполнено! Не понятна постановка задачи.';
+    ?>
+</body>
+</html>
