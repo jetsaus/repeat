02280ae65d69265c82cd6b776465e21a6ba48b9e
jetsaus <phpsaus@gmail.com>
2020-07-30 11:48:38 +0800

    40.1 Создана заготовка для решения

---
+++ 40/40.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Задача 40</title>
+</head>
+<body>
+    <h3>Задача 40:</h3>
+    <p><i>Переменная $num может принимать одно из значений: 1, 2, 3 или 4. Если она имеет значение '1', то в переменную $result запишем 'зима', если имеет значение '2' – 'лето' и так далее. Решите задачу через switch-case.</i></p>
+
+    <h3>Решение:</h3>
+    <?php
+    ?>
+</body>
+</html>
