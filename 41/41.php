f742cf55a068da2442740fbf75a011fb4f27e710
jetsaus <phpsaus@gmail.com>
2020-07-31 20:54:11 +0800

    41.1 Создана заготовка для решения

---
+++ 41/41.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Задача 41</title>
+</head>
+<body>
+    <h3>Задача 41:</h3>
+    <p><i>В переменной $day лежит какое-то число из интервала от 1 до 31. Определите в какую декаду месяца попадает это число (в первую, вторую или третью).</i></p>
+
+    <h3>Решение:</h3>
+    <?php
+        require_once('41f.php');
+    ?>
+</body>
+</html>
