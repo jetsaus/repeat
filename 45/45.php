f4cb5014744331c347ab9c5f30286846e24c31f0
jetsaus <phpsaus@gmail.com>
2020-08-04 21:21:16 +0800

    45.1 Создана заготовка для задачи

---
+++ 45/45.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Задача 45</title>
+</head>
+<body>
+    <h3>Задача 45:</h3>
+    <p><i>Дана строка с цифрами, например, '12345'. Проверьте, что первым символом этой строки является цифра 1, 2 или 3. Если это так – выведите 'да', в противном случае выведите 'нет'.</i></p>
+
+    <h3>Решение:</h3>
+    <?php
+    ?>
+</body>
+</html>
