853e20789dad37487aa199998533829d55c9282e
jetsaus <phpsaus@gmail.com>
2020-08-05 17:24:39 +0800

    46.1 Создана заготовка для решения задачи

---
+++ 46/46.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Задача 46</title>
+</head>
+<body>
+    <h3>Задача 46:</h3>
+    <p><i>Дана строка из 3-х цифр. Найдите сумму этих цифр. То есть сложите как числа первый символ строки, второй и третий.</i></p>
+
+    <h3>Решение:</h3>
+    <?php
+        include_once '46f.php';
+
+    ?>
+</body>
+</html>
