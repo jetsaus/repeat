6ef5d9b1df6452cb1c46aec5891e3340b823894d
jetsaus <phpsaus@gmail.com>
2020-08-06 13:11:28 +0800

    47.1 Создана заготовка для решения

---
+++ 47/47.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Задача 47</title>
+</head>
+<body>
+    <h3>Задача 47:</h3>
+    <p><i>Дана строка из 6-ти цифр. Проверьте, что сумма первых трех цифр равняется сумме вторых трех цифр. Если это так – выведите 'да', в противном случае выведите 'нет'.</i></p>
+
+    <h3>Решение:</h3>
+    <?php
+    ?>
+</body>
+</html>
